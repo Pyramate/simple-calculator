42f0d14bd9ecb776ebd0097f8c0b16bbc0254aee
Marie Serradori <marie.serradori@gmail.com>
2022-03-09 16:00:32 +0100

    modified script

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 // Ask a number to the player (between 1 and 100)
-const playerRandomNumber = prompt("Select a number between 1 and 100")
-const randomNumber = Math.floor(Math.random() * 100) + 1
 const playerName = prompt("What's your name?")
+let playerRandomNumber = prompt("Select a number between 1 and 100")
+playerRandomNumber=parseInt(playerRandomNumber)
+const randomNumber = Math.floor(Math.random() * 100) + 1
 
 // - If the player's number is over the stored value, log "It is less"
-function game(playerRandomNumber,randomNumber,playerName){
-
-    while (playerRandomNumber !== randomNumber){
-        if (playerRandomNumber>randomNumber){
-
-            console.log("It is less.");
-            playerRandomNumber = prompt("Select a number between 1 and 100");
-
-        } else if (playerRandomNumber<randomNumber){
-            console.log("It is more.")
-            playerRandomNumber = prompt("Select a number between 1 and 100");
-
-        } else {
-            console.log(`${playerName} wins!`)
-        }
+while (playerRandomNumber !== randomNumber) { 
+    if (playerRandomNumber>randomNumber){
+        alert("This is less")
+    } else if (playerRandomNumber<randomNumber){
+        alert("This is more")
     }
+    playerRandomNumber = prompt("Select a number between 1 and 100")
+} 
 
-}
+alert(`${playerName} win!`)
 
-game(playerRandomNumber,randomNumber,playerName)
+
+
 
 // - If the player's number is under the stored value, log "It is more"
 // - If the player's number equals the stored value, log "<player> wins" (where <player> is replaced by the player's name)
