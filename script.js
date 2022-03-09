15141fdc5bda2fe1f2b8c10b02cd97c9827aed21
Lora Perrichon <loraperrichon@gmail.com>
2022-03-09 15:54:23 +0100

    modification boucle erreurs

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 // Ask a number to the player (between 1 and 100)
-const playerRandomNumber = prompt("Select a number between 1 and 100")
+let playerRandomNumber = prompt("Select a number between 1 and 100")
+playerRandomNumber = parseInt(playerRandomNumber)
 const randomNumber = Math.floor(Math.random() * 100) + 1
 const playerName = prompt("What's your name?")
 
 // - If the player's number is over the stored value, log "It is less"
 function game(playerRandomNumber,randomNumber,playerName){
 
-    while (playerRandomNumber !== randomNumber){
-        if (playerRandomNumber>randomNumber){
+        if (playerRandomNumber == randomNumber) {
+        console.log(`${playerName} wins!`)
+        return `${playerName} wins!`
+
+        } else if {
+        while (playerRandomNumber !== randomNumber){
+            if (playerRandomNumber>randomNumber){
 
             console.log("It is less.");
             playerRandomNumber = prompt("Select a number between 1 and 100");
 
-        } else if (playerRandomNumber<randomNumber){
+            } else if (playerRandomNumber<randomNumber){
             console.log("It is more.")
             playerRandomNumber = prompt("Select a number between 1 and 100");
 
-        } else {
-            console.log(`${playerName} wins!`)
-        }
+         }
+     }
     }
-
 }
 
 game(playerRandomNumber,randomNumber,playerName)
